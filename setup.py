ab99d987b5c09b77c2ddb4ecb077c25a8f36f51e
André Gonçalves <andreslb1@gmail.com>
2014-09-12 12:29:15 +0200

    Add translation toolbar

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1.2'
+version = '0.1.3'
 
 setup(name='plonetheme.intkModern',
     version=version,
@@ -15,7 +15,7 @@ setup(name='plonetheme.intkModern',
     author='Andre Goncalves',
     author_email='andre@intk.com',
     url='https://github.com/intk/plonetheme.intkModern',
-    download_url='https://github.com/intk/plonetheme.intkModern/tarball/0.1.1',
+    download_url='https://github.com/intk/plonetheme.intkModern/tarball/0.1.3',
     license='GPL',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plonetheme'],
