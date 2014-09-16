42cf2259498c781e7cb4f1018f98cf7b586d99fb
André Gonçalves <andreslb1@gmail.com>
2014-09-16 14:25:54 +0200

    Removed event handling

--- plonetheme/intkModern/events.py
+++ plonetheme/intkModern/events.py
@@ -1,17 +1,7 @@
 
 
 def mediaObjectAdded(ob, event):
-    print "Media Object was added!"
-    
-    if not ob.checkCreationFlag():
-        print "Creating the slideshow folder."
-        ob.invokeFactory(type_name="Folder", id='slideshow', title='slideshow')
-        
-        folder = ob['slideshow']
-        folder.showinsearch = False
-        
-        folder.reindexObject()
-        try:
-            folder.portal_workflow.doActionFor(folder, "publish", comment="Slideshow content automatically published")
-        except:
-            pass
+    #
+    # Event that is triggred when a MediaPage is created
+    # Create a folder slideshow for the new MediaPage
+    #
--- plonetheme/intkModern/interfaces.py
+++
@@ -1,7 +0,0 @@
-from zope.interface import Interface
-from zope import schema
-
-class IMediaTypes(Interface):
-    """
-    Marks all Media types from Products.media*
-    """
